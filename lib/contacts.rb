7a01116ded0378c2a9e040934c6767bec33c1e60
Sarah <sarahlchavez@gmail.com>
2021-03-06 16:49:50 -0800

    commit tree d57d3fe9c2f582fc4e0964bdf78c4686a9f0dd90

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,22 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
+  # Iterate over contacts hash
+  # At :favorite_ice_cream_flavors
+  # remove "strawberry" from Freddy's favorite ice cream flavors array
+  # Set a conditional to iterate through the hash for Freddy Mercury only
+  # Check to see if the key is == to favorite_ice_cream_flavors
+  # If it is, check to see if the array of flavors contains "strawberry"
+  # If it does, then delete it from the array  
 
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
